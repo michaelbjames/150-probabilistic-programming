77cf269538108bb6e76602e90b935842f042ea27
Michael James <michael@michaelbjames.com>
2014-09-20 20:19:36 -0400

    bindx and pmap!

--- tally.hs
+++ tally.hs
@@ -8,26 +8,50 @@ regroup :: Distribution a -> Distribution a
 support :: Distribution a -> [a]
 
 equally :: [a] -> Distribution a
+weighted :: [(a, Double)] -> Distribution a
 
 probabilityOf :: Distribution a -> a -> Double
 
 pmap :: (a -> b) -> Distribution a -> Distribution b
+pmap f xs =
+    let xs' = map (\(a,b) -> (f a, b)) xs
+    in
+        regroup xs'
 
-bindx :: Distribution a -> (a -> Distribution b) -> Distribution (b,a)
-bindp :: Distribution a -> (a -> Distribution b) -> Distribution b
+bindx :: Distribution a -> (a -> Distribution b) -> Distribution (a,b)
+bindx xs distributionMap =
+    let combineDistribution (a,ap) (b,bp) = ((a,b), ap * bp)
+        bind' (a, ap) = map (combineDistribution (a, ap)) $ distributionMap a
+        allDistributions = map bind' xs 
+    in
+        foldr (++) [] allDistributions
+
+and :: Distribution a -> Distribution b -> Distribution (a,b)
+--given :: Distribution a -> Distribution b -> Distribution (a)
+
+liftp2 :: (a -> b -> c) -> Distribution a -> Distribution b -> Distribution c
 
 
+die :: Int -> Die
+sides :: Die -> Int
+dieDistribution :: Die -> Distribution Int
 
-die :: Int -> Distribution Die
-
-bag :: Distribution Die
+bagDistribution :: Distribution Die
 
 probabilityOf11 =
-    let d6 = die 6
-        d12 = die 12
+    let d6 = dieDistribution die 6
+        d12 = dieDistribution die 12
         bothDie = bindx d6 (\_ -> d12)
-        sumDistribution = pmap (uncurry (+)) bothDie
-        isEleven = pmap (==11) sumDistribution
+        distribution = pmap (uncurry (+)) bothDie
+        isEleven = pmap (==11) distribution
     in
         probabilityOf isEleven True
+
+probabilityOfD6D12 =
+    let drawDie n = pmap ((== n) . sides) bagDistribution
+        drawD6 = draw 6
+        drawD12 = draw 12
+        drawBoth = and drawD6 drawD12
+    in
+        probabilityOf drawBoth (True,True)
 
